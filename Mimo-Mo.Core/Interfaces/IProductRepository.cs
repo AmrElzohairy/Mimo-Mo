e43f27ed3c81e9b74a5dd55cad47151e2f203f67
Amr El-Zohairy <aelzohairy9@gmail.com>
2026-05-14 18:47:32 +0300

    add IProductRepository

--- Mimo-Mo.Core/Interfaces/IProductRepository.cs
+++ Mimo-Mo.Core/Interfaces/IProductRepository.cs
@@ -1,6 +1,12 @@
+using Mimo_Mo.Core.Entities;
+
 namespace Mimo_Mo.Core.Interfaces;
 
 public interface IProductRepository 
 {
-
+ Task<IEnumerable<Product>> GetAllProductsAsync();
+ Task<Product> GetProductByIdAsync(int id); 
+ Task<Product> CreateProductAsync(Product product);
+ Task<Product>   UpdateProductAsync(int id, Product product);
+ Task<Product>   DeleteProductAsync(int id);
 }
